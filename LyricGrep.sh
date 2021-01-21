30899f71941cd9c0428b726705c0bc2db35e378a
yeimsf <yeimsf@gmail.com>
2021-01-21 23:08:32 +0200

    Cleaned The Titles And Added A Readme File

--- LyricGrep.sh
+++ LyricGrep.sh
@@ -4,26 +4,12 @@
 # Author: Youssef Fakhry
 # github: https://github.com/yeimsf
 
-rm_extra_spaces() {
-    echo $@ | sed 's/^[[:space:]]*//; s/[[:space:]] *$//;
-                   #s/[[:space:]][[:space:]]*/ /g'
-}
-
 clean_search_item() {
-    local cleanup_tokens=(demo live acoustic remix bonus topic vevo video official)
-    local item=$@
-    rm_extra_spaces $item
-    item=${item//[.,?!\/\']/}        # remove special chars
-    item=${item:l}                   # lowercase
-    for token in $cleanup_tokens; do # remove tokens
-        echo ${item/\{$token\}/}
-    done
+    local cleanup_tokens=(demo live acoustic remix bonus topic vevo video official Demo Live Acoustic Remix Bonus Topic VEVO Video Official)
 }
 
 helpDoc="Help :::\n----- AVALANCHE Lyrics Finder -----\n-u <URL> | specifies a url\n-p || --mode-playlist | specifies whether to find lyrics for a playlist of songs\n-s || --mode-single | specifies whether to find lyrics for a single song\n"
 
-mode=0
-url=""
 Str_Array=($(echo $@ | tr '\b' '\n'))
 numOptions=$#
 
@@ -45,16 +31,10 @@ else
             chantitlen=`expr length "$jsonResChanTitle" - 27`
             song=${jsonResVidTitle:18:$vidtitlen}
             artist=${jsonResChanTitle:25:$chantitlen}
-            #artist=$(echo $jsonResChanTitle | tr ' ' '-')
-            #song=$(echo $jsonResVidTitle | tr ' ' '-')
-            artist=$(clean_search_item $artist) 
-            song=$(clean_search_item $song)
+            artist=$(echo $artist | tr '[:upper:]' '[:lower:]' | sed -e 's/([^()]*)//g' | sed -e 's/[[^[]]*]//g' | sed -e 's/vevo//g' | sed -e 's/video//g' | sed -e 's/remix//g' | sed -e 's/vevo//g' | sed -e 's/topic//g')
+            song=$(echo $song | tr '[:upper:]' '[:lower:]' | sed -e 's/([^()]*)//g' | sed -e 's/[[^[]]*]//g' | sed -e 's/vevo//g' | sed -e 's/video//g' | sed -e 's/remix//g' | sed -e 's/vevo//g' | sed -e 's/topic//g')
             echo $artist
             echo $song
-            #channtitle=$(echo $jsonRes 2>&1 | grep 'channelTitle')
-            #echo -e "$vidtitle\n$channtitle"
-            #echo $(curl https://www.googleapis.com/youtube/v3/videos?key=AIzaSyDagL3KHQTkMiDHfrz2i4Fyl_iqua2le3A&part=snippet&id=$vidid)
-            #echo $(title=wget -qO- ${url} |  perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)(?: - youtube)?\s*<\/title/si')
             exit
         else
             echo -e "Invalid argument - See help page '-h'"
---
+++ README.md
@@ -0,0 +1,17 @@
+## Avalanche Lyrics Finder
+
+
+### This Tool Is Used For Searching On A Specific Song's Lyrics
+
+### This Tool Requires The Latest Version Of Googler To Be Installed In /usr/local
+#### Help Manual:
+
+```
+Help :::
+----- AVALANCHE Lyrics Finder -----
+-u <URL> | specifies a url
+-p || --mode-playlist | specifies whether to find lyrics for a playlist of songs {Work In Progress}
+-s || --mode-single | specifies whether to find lyrics for a single song {Work In Progress}
+```
+
+- Developer [Youssef Fakhry](https://github.com/yeimsf)
