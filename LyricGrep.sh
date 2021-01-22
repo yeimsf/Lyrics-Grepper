c900bfbf61e9f7060682d9a01df09d35944829c0
yeimsf <yeimsf@gmail.com>
2021-01-22 12:06:51 +0200

    Finished But With Opening The Lyrics Website

--- LyricGrep.sh
+++ LyricGrep.sh
@@ -4,10 +4,6 @@
 # Author: Youssef Fakhry
 # github: https://github.com/yeimsf
 
-clean_search_item() {
-    local cleanup_tokens=(demo live acoustic remix bonus topic vevo video official Demo Live Acoustic Remix Bonus Topic VEVO Video Official)
-}
-
 helpDoc="Help :::\n----- AVALANCHE Lyrics Finder -----\n-u <URL> | specifies a url\n-p || --mode-playlist | specifies whether to find lyrics for a playlist of songs\n-s || --mode-single | specifies whether to find lyrics for a single song\n"
 
 Str_Array=($(echo $@ | tr '\b' '\n'))
@@ -25,17 +21,32 @@ else
         then
             url=${Str_Array[counter + 1]}
             vidid=${url:17:11}
-            jsonResVidTitle=$(curl -G -s https://www.googleapis.com/youtube/v3/videos -d part=snippet -d key="AIzaSyDagL3KHQTkMiDHfrz2i4Fyl_iqua2le3A" -d id="$vidid" | grep -m 1 'title')
-            jsonResChanTitle=$(curl -G -s https://www.googleapis.com/youtube/v3/videos -d part=snippet -d key="AIzaSyDagL3KHQTkMiDHfrz2i4Fyl_iqua2le3A" -d id="$vidid" | grep -m 1 'channelTitle')
+            jsonResVidTitle=$(curl -G -s https://www.googleapis.com/youtube/v3/videos -d part=snippet -d key="AIzaSyDagL3KHQTkMiDHfrz2i4Fyl_iqua2le3A" -d id="$vidid" | tac | tac | grep -m 1 'title')
+            jsonResChanTitle=$(curl -G -s https://www.googleapis.com/youtube/v3/videos -d part=snippet -d key="AIzaSyDagL3KHQTkMiDHfrz2i4Fyl_iqua2le3A" -d id="$vidid" | tac | tac | grep -m 1 'channelTitle')
             vidtitlen=`expr length "$jsonResVidTitle" - 20`
             chantitlen=`expr length "$jsonResChanTitle" - 27`
             song=${jsonResVidTitle:18:$vidtitlen}
             artist=${jsonResChanTitle:25:$chantitlen}
-            artist=$(echo $artist | tr '[:upper:]' '[:lower:]' | sed -e 's/([^()]*)//g' | sed -e 's/[[^[]]*]//g' | sed -e 's/vevo//g' | sed -e 's/video//g' | sed -e 's/remix//g' | sed -e 's/vevo//g' | sed -e 's/topic//g')
-            song=$(echo $song | tr '[:upper:]' '[:lower:]' | sed -e 's/([^()]*)//g' | sed -e 's/[[^[]]*]//g' | sed -e 's/vevo//g' | sed -e 's/video//g' | sed -e 's/remix//g' | sed -e 's/vevo//g' | sed -e 's/topic//g')
-            echo $artist
-            echo $song
-            exit
+            artist=$(echo $artist | tr '[:upper:]' '[:lower:]' | sed -e 's/([^()]*)//g' | sed -e 's/[[^[]]*]//g' | sed -e 's/vevo//g' | sed -e 's/video//g' | sed -e 's/remix//g' | sed -e 's/vevo//g' | sed -e 's/topic//g' | sed -e 's/music//g' | sed -e 's/official//g' | sed -e 's/-//g')
+            song=$(echo $song | tr '[:upper:]' '[:lower:]' | sed -e 's/([^()]*)//g' | sed -e 's/[[^[]]*]//g' | sed -e 's/vevo//g' | sed -e 's/video//g' | sed -e 's/remix//g' | sed -e 's/vevo//g' | sed -e 's/topic//g' | sed -e 's/music//g' | sed -e 's/official//g' | sed -e 's/-//g')
+            echo "=================================================="
+            echo -e "The Artist Is:$artist\nThe Song Is: $song"
+            echo "=================================================="
+            echo "Is That True? (Y/N) -> "
+            read confirmation
+            if [ "${confirmation}" = 'Y' ]
+            then
+                template="$artist- $song"
+                echo $(~/Documents/googler/googler/googler "$template" -j -w https://www.musixmatch.com/lyrics -n 1)   
+                exit
+            else
+                echo -en "---------- Please Submit An Issue At https://github.com/yeimsf/Lyrics-Grepper ----------\nTermination."
+                sleep 0.8
+                echo -n "."
+                sleep 0.8
+                echo "."
+                exit
+            fi
         else
             echo -e "Invalid argument - See help page '-h'"
             exit
